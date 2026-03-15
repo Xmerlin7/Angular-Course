683a986e44a992983771d86299011dbe4c675d2a
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-15 04:18:51 +0200

    feat: implement routing and add contact component with form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/slider/slider').then((component) => component.Slider),
+  },
+  {
+    path: 'product',
+    loadComponent: () => import('./components/cards/cards').then((component) => component.Cards),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./components/contact/contact').then((component) => component.Contact),
+  },
+  { path: '**', redirectTo: 'slider' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 import { Cards } from './components/cards/cards';
 import { Slider } from './components/slider/slider';
+import { RouterOutlet } from "@angular/router";
 @Component({
   selector: 'app-root',
-  imports: [Footer, Navbar, Cards, Slider],
+  imports: [Footer, Navbar, Cards, Slider, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
