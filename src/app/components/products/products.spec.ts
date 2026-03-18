deaa6d027ac15749027be9616a185e6af073e65e
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-18 17:33:38 +0200

    test: add ProductService mock to Products component tests

--- src/app/components/products/products.spec.ts
+++ src/app/components/products/products.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { Products } from './products';
+import { ProductService } from '../../services/product';
 
 describe('Products', () => {
   let component: Products;
   let fixture: ComponentFixture<Products>;
 
   beforeEach(async () => {
+    const productServiceSpy = {
+      getProducts: () => of([]),
+      createProduct: () => of(),
+      updateProduct: () => of(),
+      deleteProduct: () => of(),
+    };
+
     await TestBed.configureTestingModule({
       imports: [Products],
+      providers: [{ provide: ProductService, useValue: productServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Products);
